ece01f5e87112263303f51791153cbaae7a091b9
Gautam Kishore Shahi <gautamshahi16@gmail.com>
2021-11-15 01:55:49 +0100

    dsss assigment files

---
+++ function.py
@@ -0,0 +1,3 @@
+import numpy as np
+from ipywidgets import interact, fixed
+from PIL import Image
---
+++ setup.py.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='',
+      version ='0.1',
+      author='Sushma'
+      author_email='sush.kumari@fau.de'
+      packages=find_packages(),
+      install_requires=['numpy','Pillow','ipywidgets'])
