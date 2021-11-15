b70e8c6ca88dc5e142b41c9bf402e51c8b0dbbc7
Gautam Kishore Shahi <gautamshahi16@gmail.com>
2021-11-15 02:01:54 +0100

    dsss assigment files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(name='',
       version ='0.1',
-      author='Sushma'
+      author='Sushma',
       author_email='sush.kumari@fau.de',
       packages=find_packages(),
       install_requires=['numpy','Pillow','ipywidgets'])
