34af7f3a3b5316f1aa9c8d859496cae20dafe850
Gautam Kishore Shahi <gautamshahi16@gmail.com>
2021-11-15 03:15:44 +0100

    dsss assigment files

--- idm/function.py
+++ idm/function.py
@@ -3,16 +3,6 @@ from ipywidgets import interact, fixed
 from PIL import Image
 import matplotlib.pyplot as plt
 
-'''
-def browse_images(digits):
-    n = len(digits.images)
-    def view_image(i):
-        plt.imshow(digits.images[i], cmap=plt.cm.gray_r, interpolation='nearest')
-        plt.title('Training: %s' % digits.target[i])
-        plt.show()
-    interact(view_image, i=(0,n-1))
-    
- '''   
 
 def imshow(X, resize=None):
     print('ef')
