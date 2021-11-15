ff1131158c417b2173618b68ba33352bb5be333f
Gautam Kishore Shahi <gautamshahi16@gmail.com>
2021-11-15 03:36:05 +0100

    dsss assigment files

--- idm/function.py
+++ idm/function.py
@@ -4,8 +4,7 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 
-def imshow_image(X, resize=None):
-
+def imshow_img(X, resize=None):
     res = cv2.resize(X, (54, 140), interpolation=cv2.INTER_CUBIC)
     plt.imshow(res) #code to print the image
     pass
