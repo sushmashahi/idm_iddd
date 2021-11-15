70b427af72f71e1abf75f335e7f68bfa8f0b1469
Gautam Kishore Shahi <gautamshahi16@gmail.com>
2021-11-15 03:18:33 +0100

    dsss assigment files

--- idm/function.py
+++ idm/function.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def imshow(X, resize=None):
-    print('ef')
+    print("kjndkf")
     img = np.resize(X) #code to resize the image
     print(plt.imshow(img)) #code to print the image
     #browse_images(img) #to diplay the image interact using matplotlib and ipywidgets
